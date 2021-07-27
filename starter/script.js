648205f440370d257728e82396e0ed36f3913bef
Sandeep Pal <sandeepdtu360@gmail.com>
2021-07-28 00:01:37 +0530

    first

---
+++ starter/script.js
@@ -0,0 +1,106 @@
+'use strict';
+
+const btn = document.querySelector('.btn-country');
+const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = function (data, className = '') {
+    const html = `
+  <article class="country ${className}">
+    <img class="country__img" src="${data.flag}" />
+    <div class="country__data">
+      <h3 class="country__name">${data.name}</h3>
+      <h4 class="country__region">${data.region}</h4>
+      <p class="country__row"><span>👫</span>${(
+            +data.population / 1000000
+        ).toFixed(1)} people</p>
+      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
+    </div>
+  </article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+};
+const renderError = function (msg) {
+    countriesContainer.insertAdjacentText('beforeend', msg);
+    countriesContainer.style.opacity = 1;
+};
+/*
+// using call back
+const getCountryAndNeighbour = function (country) {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://restcountries.eu/rest/v2/name/${country}`);
+    request.send();
+
+    request.addEventListener('load', function () {
+        //Ajax call
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+        //Render country 1
+        renderCountry(data);
+        //Get neighbour country(2)
+        const [neighbour] = data.borders;
+        if (!neighbour) return;
+        //Ajax call country 2:
+        const request2 = new XMLHttpRequest();
+        request2.open('GET', `https://restcountries.eu/rest/v2/alpha/${neighbour}`); /*callback happening here*/
+/*
+request2.send();
+request2.addEventListener('load', function () {
+    const data2 = JSON.parse(this.responseText);
+    renderCountry(data2, 'neighbour');
+})
+});
+};
+/*
+getCountryAndNeighbour('bharat');
+getCountryAndNeighbour('usa');
+*/
+// Promise ______________________________________________
+//1-fetch function return a promise afterthat we handle using .then method 
+//to read a response of json , we return a response.json --> then its become new promise, to handle this promise again we .then method
+
+// const getCountryData = function (country) {
+//     fetch(`https://restcountries.eu/rest/v2/name/${country}`).then(function (response) {
+//         console.log(response);
+//         return response.json();
+
+//     }).then(function (data) {
+//         console.log(data);
+//         renderCountry(data[0]);
+//     });
+// };
+//simplified version of above code with arrow function:
+
+const getCountryData = function (country) {
+    fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+        .then((response) => {
+            if (!response.ok)
+                throw new Error(`Country not found (${response.status})`);
+            return response.json();
+        })
+        .then((data) => {
+            renderCountry(data[0]);
+            const neighbour = data[0].borders[0];
+            if (!neighbour) return;
+            // country 2
+            return fetch(`https://restcountries.eu/rest/v2/alpha/${neighbour}`)
+        })
+        // then method is  only called when promise fullfill
+        // catch method is only called when promise reject
+        // finally is not mostly we use this method that always to be happen no matter promise fullfill or reject. Ex- To hide loading spinner.
+        .then(response => response.json())
+        .then(data => renderCountry(data, 'neighbour'))
+        .catch(err => {
+            console.error(`${err} 💥💥💥`);
+            renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+        })
+        .finally(() => {
+            countriesContainer.style.opacity = 1;
+        });
+
+};
+btn.addEventListener('click', function () {
+    getCountryData('bharat');
+})
+// check with wrong  getCountryData('sddwdwcsaxqx');
